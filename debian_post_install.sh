2b65b27fa8aa0abb6f66e12dcdaaf3828ddfbe84
cdbdev <39485622+cdbdev@users.noreply.github.com>
2019-08-07 21:06:47 +0200

    Update debian_post_install.sh

--- debian_post_install.sh
+++ debian_post_install.sh
@@ -13,4 +13,7 @@
 # ----------------------------------------------- 
 
 echo ":: Optimizing kernel for display..."
-
+```
+# sed -i '/GRUB_CMDLINE_LINUX_DEFAULT=/c\GRUB_CMDLINE_LINUX_DEFAULT=\"quiet acpi_backlight=none amdgpu.dc=0\"' /etc/default/grub
+# grub-mkconfig -o /boot/grub/grub.cfg
+```
