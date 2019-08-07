186303c8d533263ac00fcca11912e0a8e6bfd1c8
cdbdev <39485622+cdbdev@users.noreply.github.com>
2019-08-07 21:51:50 +0200

    Update debian_post_install.sh

--- debian_post_install.sh
+++ debian_post_install.sh
@@ -12,6 +12,10 @@
 # Fix dark screen & hibernate
 # ----------------------------------------------- 
 
-echo ":: Optimizing kernel for display..."
+echo ":: Adjusting GRUB for optimal display..."
 sed -i '/GRUB_CMDLINE_LINUX_DEFAULT=/c\GRUB_CMDLINE_LINUX_DEFAULT=\"quiet acpi_backlight=none amdgpu.dc=0\"' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
+
+# ----------------------------------------------- 
+# Fix dark screen & hibernate
+# ----------------------------------------------- 
