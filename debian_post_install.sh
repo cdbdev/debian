c593728d766ccfda1f6cef3e537634bd7e0379cb
cdbdev <39485622+cdbdev@users.noreply.github.com>
2019-08-08 08:22:02 +0200

    Update debian_post_install.sh

--- debian_post_install.sh
+++ debian_post_install.sh
@@ -7,11 +7,14 @@
 # Project   : https://github.com/cdbdev/debian-install			  #
 # ----------------------------------------------------------------------- #
 
+# ----------------------------------------------- 
+# Update system
+# ----------------------------------------------- 
+apt-get update && apt-get upgrade
 
 # ----------------------------------------------- 
 # Fix dark screen & hibernate
 # ----------------------------------------------- 
-
 echo ":: Adjusting GRUB for optimal display..."
 sed -i '/GRUB_CMDLINE_LINUX_DEFAULT=/c\GRUB_CMDLINE_LINUX_DEFAULT=\"quiet acpi_backlight=none amdgpu.dc=0\"' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
@@ -19,6 +22,7 @@ grub-mkconfig -o /boot/grub/grub.cfg
 # ----------------------------------------------- 
 # Install and configure nftables
 # ----------------------------------------------- 
-apt install nftables
+echo ":: Installing nftables..."
+apt-get install nftables
 mv conf/nftables.conf /etc/
 systemctl enable nftables.service
