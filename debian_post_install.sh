d8c570b77d025fface952c5bf51dd35f91f892ce
cdbdev <39485622+cdbdev@users.noreply.github.com>
2019-09-17 16:11:50 +0200

    Update debian_post_install.sh

--- debian_post_install.sh
+++ debian_post_install.sh
@@ -41,7 +41,7 @@ echo screenfetch >> .bashrc
 # ----------------------------------------------- 
 echo ":: Adjusting GRUB for optimal display..."
 sed -i '/GRUB_CMDLINE_LINUX_DEFAULT=/c\GRUB_CMDLINE_LINUX_DEFAULT=\"quiet acpi_backlight=none amdgpu.dc=0\"' /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg
+update-grub
 
 # ----------------------------------------------- 
 # Install and configure nftables
